96546087cb3431e97bd66cb0cba66bcc5f5f7b25
Chyngyz <camphosis@gmail.com>
2015-12-14 11:41:47 +0600

    Mail CC added

--- mailCorp.php
+++ mailCorp.php
@@ -4,7 +4,8 @@ $EmailFrom = "bravo-bishkek@mail.ru";
 $EmailTo = "bravo-bishkek@mail.ru";
 $Subject = "Заявка с сайта на заказ банкета";
 $Name = Trim(stripslashes($_POST['name'])); 
-$Tel = Trim(stripslashes($_POST['phone'])); 
+$Tel = Trim(stripslashes($_POST['phone']));
+$headers = 'Cc: frolova.project@gmail.com' . "\r\n";
 
 
 // prepare email body text
